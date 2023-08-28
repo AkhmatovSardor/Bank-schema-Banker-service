db182c000530fa5e582e8f9cd51721fa6a07e088
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-08-28 14:27:51 +0500

    first commit

---
+++ src/main/java/com/company/Bankbankerservice/BankBankerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.Bankbankerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBankerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBankerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Bankbankerservice/BankBankerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Bankbankerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBankerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
